7f3f9cf7ea9c99e016a61a97ad54d78dd9416061
Andrew <90246667+Papsanly@users.noreply.github.com>
2024-02-22 15:11:26 +0200

    Add models.py and authorization.py

---
+++ lab1/authorization.py
@@ -0,0 +1,100 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+# ========================================================= #
+
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from fastapi import Depends, HTTPException, status, APIRouter
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+SECRET_KEY = "secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# ========================================================= #
+
+from sqlalchemy.orm import Session
+from models import User 
+
+def authenticate_user(db: Session, username: str, password: str) -> User:
+    user = db.query(User).filter(User.username == username).first()
+    if user and verify_password(password, user.hashed_password):
+        return user
+    return None
+
+
+def create_superadmin(db: Session):
+    superadmin_email = "superadmin@example.com"
+    superadmin = db.query(User).filter(User.email == superadmin_email).first()
+    if not superadmin:
+        superadmin_user = User(
+            username="superadmin",
+            email=superadmin_email,
+            hashed_password=get_password_hash("superadminpassword"),
+            role="admin"
+        )
+        db.add(superadmin_user)
+        db.commit()
+
+def create_user(db: Session):
+    superadmin_email = "user@example.com"
+    superadmin = db.query(User).filter(User.email == superadmin_email).first()
+    if not superadmin:
+        superadmin_user = User(
+            username="user",
+            email=superadmin_email,
+            hashed_password=get_password_hash("userpassword"),
+            role="user"
+        )
+        db.add(superadmin_user)
+        db.commit()
+
+# =============================================== #
+
+from models import get_db 
+        
+def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        user = db.query(User).filter(User.username == username).first()
+        if user is None:
+            raise credentials_exception
+        return user
+    except JWTError:
+        raise credentials_exception
+
+def get_superadmin(user: User = Depends(get_current_user)):
+    if user.role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="The user doesn't have enough privileges"
+        )
+    return user
---
+++ lab1/models.py
@@ -0,0 +1,99 @@
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    role = Column(String, default="user")
+
+    def __init__(self, username, email, hashed_password, role="user"):
+        self.username = username
+        self.email = email
+        self.hashed_password = hashed_password
+        self.role = role
+
+    def __repr__(self):
+        return self.username
+
+
+class Country(Base):
+    __tablename__ = 'countries'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True)
+    code = Column(String, unique=True)
+
+    def __init__(self, name, code):
+        self.code = code
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+
+class City(Base):
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    country_id = Column(Integer, ForeignKey('countries.id'))
+
+    def __init__(self, name, country_id):
+        self.name = name
+        self.country_id = country_id
+
+    def __repr__(self):
+        return f'City(name={self.name}, country_id={self.country_id})'
+
+
+class Forecast(Base):
+    __tablename__ = 'forecasts'
+
+    id = Column(Integer, primary_key=True, index=True)
+    city_id = Column(Integer, ForeignKey('cities.id'), name='city_id_fk')
+    datetime = Column(DateTime)
+    forecasted_temperature = Column(Float)
+    forecasted_humidity = Column(Float)
+
+    def __init__(self, datetime, city_id, forecasted_temperature, forecasted_humidity):
+        self.datetime = datetime
+        self.city_id = city_id
+        self.forecasted_temperature = forecasted_temperature
+        self.forecasted_humidity = forecasted_humidity
+
+
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class ForecastResponse(BaseModel):
+    city_id: int
+    datetime: datetime
+    forecasted_temperature: float
+    forecasted_humidity: float
+
+
+class ForecastMessage(BaseModel):
+    message: str
+
+
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
